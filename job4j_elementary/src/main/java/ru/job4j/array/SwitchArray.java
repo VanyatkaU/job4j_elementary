d0c93d35734e908844e4d1d36f4fdfc5ddd3f671
VanyatkaU <ivanKa49@ya.ru>
2021-09-08 16:38:06 +0300

    add maven

--- job4j_elementary/src/main/java/ru/job4j/array/SwitchArray.java
+++ job4j_elementary/src/main/java/ru/job4j/array/SwitchArray.java
@@ -20,8 +20,8 @@ public class SwitchArray {
     public static void main(String[] args) {
         int[] nums = new int[]{1, 2, 3, 4, 5, 6};
         int[] rsl = swapBorder(nums);
-        for (int j : rsl) {
-            System.out.println(j);
+        for (int index = 0; index < rsl.length; index++) {
+            System.out.println(rsl[index]);
             for (int i : rsl) {
                 System.out.println(i);
             }
