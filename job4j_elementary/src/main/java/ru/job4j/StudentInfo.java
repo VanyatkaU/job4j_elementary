4ccc3afbc5531572dc3ae4d85b4ceae0a2063d69
VanyatkaU <88547613+VanyatkaU@users.noreply.github.com>
2023-01-05 00:33:34 +0300

    Delete job4j_elementary/src directory

--- job4j_elementary/src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Ivan");
-        System.out.println("08.03.1985");
-
-    }
-}
